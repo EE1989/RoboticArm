383b71e83f9d9130f0a44569527e3c9f48f1099b
Emre <emre.eker@ymail.com>
2018-01-28 16:44:19 +0300

    unused references deleted

--- src/com/robotic/App.java
+++ src/com/robotic/App.java
@@ -1,11 +1,5 @@
 package com.robotic;
 
-import java.awt.*;
-import java.awt.geom.*;
-import java.awt.event.*;
-import javax.swing.*;
-import javax.swing.event.*;
-
 public class App {
 	public static void main(String[] arg)
 	{
