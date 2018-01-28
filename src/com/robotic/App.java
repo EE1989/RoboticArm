8347010983e5cab5fcab163599688b47a34df5b8
Emre <emre.eker@ymail.com>
2018-01-28 16:42:12 +0300

    Files added

---
+++ src/com/robotic/App.java
@@ -0,0 +1,14 @@
+package com.robotic;
+
+import java.awt.*;
+import java.awt.geom.*;
+import java.awt.event.*;
+import javax.swing.*;
+import javax.swing.event.*;
+
+public class App {
+	public static void main(String[] arg)
+	{
+		Gui gui = new Gui();
+	}
+}
